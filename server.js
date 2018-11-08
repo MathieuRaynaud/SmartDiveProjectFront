f12104fd7720c129449aa1f9d215353c133fb222
Raynaud <mraynaud@etud.insa-toulouse.fr>
2018-11-08 21:36:09 +0100

    Added Google Maps API

--- server.js
+++ server.js
@@ -1,44 +1,27 @@
-var http = require('http');
 var test_module = require('test_module');
 var EventEmitter = require('events').EventEmitter;
 var express = require('express');
 
 var app = express();
-var map = new EventEmitter();
+var app_logs = new EventEmitter();
 
-/*var server = http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('<!DOCTYPE html>'+
-        '<html>'+
-        '    <head>'+
-        '        <meta charset="utf-8" />'+
-        '        <title>Smart Dive Project</title>'+
-        '    </head>'+
-        '    <body>'+
-        '     	<h1>Smart Dive Project</h1>'+
-        '    </body>'+
-        '</html>');
-    res.end();
-});
-
-server.listen(8080);*/
+app.use(express.static('style'));
 
 app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil');
+    res.setHeader('Content-Type', 'text/html');
+    res.render('map.ejs');
 });
 
-app.get('/etage/:etagenum/chambre', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+/******************/
+/***** Events *****/
+/******************/
+
+app_logs.on('listening', function(){
+    console.log('App listening at localhost:8080');
 });
 
-app.listen(8080);
-
-map.on('pin_clicked', function(message){
-    console.log(message);
-});
-
-map.emit('pin_clicked', 'Vous avez cliqué sur un pin !');
+if(app.listen(8080)){
+    app_logs.emit('listening');
+}
 
 test_module.direBonjour();
